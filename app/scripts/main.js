46dfec287b1215a0164f8962f1f9197202ac7b92
Mike Zevitas <mzevitas@gmail.com>
2015-09-04 15:01:26 -0400

    slider test

---
+++ app/scripts/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function ($) {
+        var options = { 
+
+        	$AutoPlay: true,
+        	 $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$,
+                $ChanceToShow: 2
+            } 
+
+
+        };
+        var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+    });
