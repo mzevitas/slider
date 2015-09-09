931ec38cf6c5623cf4ab567e739189a51083ee2e
Mike Zevitas <mzevitas@gmail.com>
2015-09-09 11:16:37 -0400

    commiting for stuff

---
+++ app/scripts/widget.php
@@ -0,0 +1,13 @@
+<?php
+
+class nexidia_form extends WP_Widget {
+
+// constructor
+function nexidia_form() {
+// Give widget name here
+parent::WP_Widget(false, $name = __('Nexidia Form', 'wp_widget_plugin') );
+
+}
+
+
+?>
